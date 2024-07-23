75e6f15cf5e76ad0ea9f092efc2d825eaa1386fa
Jakub Molinski <jakubmolinski9@gmail.com>
2024-07-23 19:10:56 +0200

    App is kind of working - need to fix a bug that occurs when adding books to the screen

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
 const toggleDialog = document.querySelector(".dialog-toggle");
 const sidebar = document.querySelector("#sidebar");
 const closeBtn = document.querySelector(".close-dialog");
-const addBookBtn = document.querySelector(".add-btn ");
+const addBookBtn = document.querySelector(".add-btn");
+const form = document.querySelector("form");
+const bookTitle = document.querySelector("#title");
+const bookAuthor = document.querySelector("#author");
+const bookPages = document.querySelector("#pages");
 
-function Book(title, author, pages, read) {
+const myLibrary = [];
+
+function Book(title, author, pages) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read;
 }
+
+function addBookToLibrary() {
+  const book = new Book(bookTitle.value, bookAuthor.value, bookPages.value);
+  myLibrary.push(book);
+}
+
+function displayBook() {
+  myLibrary.forEach((book) => {
+    const bookList = document.querySelector(".book-list");
+    const { title, author, pages } = book;
+    bookList.innerHTML += `<div class="card">
+              <h3>Title: ${title}</h3>
+              <h3>Author: ${author}</h3>
+              <h3>Number of pages: ${pages}</h3>
+             </div>`;
+  });
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+addBookBtn.addEventListener("click", () => {
+  addBookToLibrary();
+  displayBook();
+});
 
 toggleDialog.addEventListener("click", () => {
   sidebar.style.width = "250px";
